e8ebf117b144e080387ba342117e9eeac765af84
RosyGraph <46096905+RosyGraph@users.noreply.github.com>
2023-05-09 09:31:34 -0600

    Deploying to gh-pages from @ RosyGraph/RosyGraph.github.io@c5ba6deccc422ac35a1e2d3fa2348b0364df8641 🚀

---
+++ _next/static/CUe4eN6bW1rQ3lYRb9yX7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/806-04bc12ffb7b1b78e.js","static/chunks/pages/index-f5e81b66d03296d8.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/d4i3y5wSCrwlKDokBwNlE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/728-640cae9c98a2b58b.js","static/chunks/pages/index-b0c04acbf53d66f4.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
