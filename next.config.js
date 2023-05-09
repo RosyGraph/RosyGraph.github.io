aab234030b1a8f68b913843453d60774e5639a8e
RosyGraph <chris.stevenson@live.com>
2023-05-08 20:42:38 -0600

    Initialize Next app

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*\//, "");
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+module.exports = {
+  assetPrefix,
+  basePath,
+};
