7db6cb24554d08b164f76b278ec849c07105cbac
RosyGraph <46096905+RosyGraph@users.noreply.github.com>
2023-05-08 22:11:50 -0600

    Deploying to gh-pages from @ RosyGraph/RosyGraph.github.io@57cec1acf37d4abf8944d567cd05b3910fba7dae 🚀

--- _next/static/HHDnCcqACtYw10M6APG3V/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/308-6fea01ef2451cfff.js","static/chunks/pages/index-f80a4eac9ce41472.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
