c827e982a33a9cd42cf98db96b3daed3b65a6263
RosyGraph <chris.stevenson@live.com>
2023-05-12 21:52:31 -0600

    Remove github action; memoize usetheme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-
-const theme = createTheme({
-  typography: {
-    fontFamily: "Roboto Mono, monospace",
-  },
-});
+import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        typography: {
+          fontFamily: "Roboto Mono, monospace",
+        },
+      }),
+    []
+  );
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
