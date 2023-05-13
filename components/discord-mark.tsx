099389e37e1139c91a3c4d0afb3b1829f18e8c35
RosyGraph <chris.stevenson@live.com>
2023-05-12 22:50:09 -0600

    Swap SVGs for components

--- components/discord-mark.tsx
+++ components/discord-mark.tsx
@@ -1,15 +1,35 @@
 import Link from "next/link";
-import Image from "next/image";
+import Box from "@mui/material/Box";
+import { useTheme } from "@mui/material";
+
+function DiscordMarkSVG({ fill }: { fill: string }) {
+  return (
+    <svg width="100%" height="100%" viewBox="0 0 128 128">
+      <path
+        fill={fill}
+        d="M107.7,8.07A105.15,105.15,0,0,0,81.47,0a72.06,72.06,0,0,0-3.36,6.83A97.68,97.68,0,0,0,49,6.83,72.37,72.37,0,0,0,45.64,0,105.89,105.89,0,0,0,19.39,8.09C2.79,32.65-1.71,56.6.54,80.21h0A105.73,105.73,0,0,0,32.71,96.36,77.7,77.7,0,0,0,39.6,85.25a68.42,68.42,0,0,1-10.85-5.18c.91-.66,1.8-1.34,2.66-2a75.57,75.57,0,0,0,64.32,0c.87.71,1.76,1.39,2.66,2a68.68,68.68,0,0,1-10.87,5.19,77,77,0,0,0,6.89,11.1A105.25,105.25,0,0,0,126.6,80.22h0C129.24,52.84,122.09,29.11,107.7,8.07ZM42.45,65.69C36.18,65.69,31,60,31,53s5-12.74,11.43-12.74S54,46,53.89,53,48.84,65.69,42.45,65.69Zm42.24,0C78.41,65.69,73.25,60,73.25,53s5-12.74,11.44-12.74S96.23,46,96.12,53,91.08,65.69,84.69,65.69Z"
+      />
+    </svg>
+  );
+}
 
 export default function DiscordMark() {
+  const theme = useTheme();
+
   return (
     <Link href="https://discordapp.com/users/RosyGraph#1240">
-      <Image
-        src="/discord-mark-black.png"
-        alt="Discord Mark"
-        width={32}
-        height={24}
-      />
+      <Box
+        sx={{
+          width: 48,
+          height: 36,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          cursor: "pointer",
+        }}
+      >
+        <DiscordMarkSVG fill={theme.palette.primary.contrastText} />
+      </Box>
     </Link>
   );
 }
