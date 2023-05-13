c857c3b68f6bf90fb4716e2d69e5d216d93f058d
RosyGraph <chris.stevenson@live.com>
2023-05-13 09:40:36 -0600

    Update favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import CssBaseline from "@mui/material/CssBaseline";
 import { AppProps } from "next/dist/shared/lib/router/router";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
@@ -21,9 +22,15 @@ export default function App({ Component, pageProps }: AppProps) {
     []
   );
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>Chris Stevenson</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
